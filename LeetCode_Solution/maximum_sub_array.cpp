dad631b16cdbb31a96db3b929ea81d9a9475db95
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-08 01:27:31 +0530

    maximumSumArray

---
+++ LeetCode_Solution/maximum_sub_array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maximumSubArray(vector<int>&v){
+    int maxi = INT_MIN;
+    for(int i = 0; i<v.size(); i++){
+        int sum = 0;
+        for(int j = i;j<v.size(); j++){
+            sum = sum+v[j];
+
+            maxi = max(maxi, sum);
+        }
+    }
+    return maxi;
+}
+int main()
+{
+    vector<int> v={-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout<<maximumSubArray(v);
+   
+   return 0;
+}
