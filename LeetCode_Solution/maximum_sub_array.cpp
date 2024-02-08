439a2448d33b341b56d097f275cb28c0f0c4b122
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-09 00:33:18 +0530

    O(n) maximum sub array (Kadane Algorithm)

--- LeetCode_Solution/maximum_sub_array.cpp
+++ LeetCode_Solution/maximum_sub_array.cpp
@@ -1,22 +1,54 @@
-#include<bits/stdc++.h>
-using namespace std;
-int maximumSubArray(vector<int>&v){
-    int maxi = INT_MIN;
-    for(int i = 0; i<v.size(); i++){
-        int sum = 0;
-        for(int j = i;j<v.size(); j++){
-            sum = sum+v[j];
+// #include <bits/stdc++.h>
+// using namespace std;
+// int maximumSubArray(vector<int> &v)
+// {
+//     int maxi = INT_MIN;
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         int sum = 0;
+//         for (int j = i; j < v.size(); j++)
+//         {
+//             sum = sum + v[j];
 
-            maxi = max(maxi, sum);
+//             maxi = max(maxi, sum);
+//         }
+//     }
+//     return maxi;
+// }
+// int main()
+// {
+//     vector<int> v = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//     cout << maximumSubArray(v);
+
+//     return 0;
+// }
+// kadane algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int maxi = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum = sum + nums[i];
+            maxi = max(sum, maxi);
+            if (sum < 0)
+            {
+                sum = 0;
+            }
         }
+        return maxi;
     }
-    return maxi;
-}
+};
 int main()
 {
-    vector<int> v={-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    cout<<maximumSubArray(v);
-   
-   return 0;
+    Solution s;
+    vector<int> v = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << s.maxSubArray(v);
+    return 0;
 }
-// kadane algorithm
