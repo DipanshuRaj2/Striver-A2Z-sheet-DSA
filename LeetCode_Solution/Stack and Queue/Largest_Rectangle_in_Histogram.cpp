6d3a1520658c4a4eb426af1e4887e920cc442964
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-21 11:36:44 +0530

    using stack Largest_Rectangle_in Histogram

--- LeetCode_Solution/Stack and Queue/Largest_Rectangle_in_Histogram.cpp
+++ LeetCode_Solution/Stack and Queue/Largest_Rectangle_in_Histogram.cpp
@@ -9,31 +9,92 @@ Input: heights = [2,1,5,6,2,3]
 Output: 10
 Explanation: The above is a histogram where width of each bar is 1.
 The largest rectangle is shown in the red area, which has an area = 10 units.
-naive approach using array*/
+
+
+
+
+                                     naive approach using array*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution{
+//     public:
+//     int largestRectangleArea(vector<int>& arr){
+//         int res = 0;
+//         for(int i = 0; i<arr.size(); i++){
+//             int curr = arr[i];
+//             for(int j = i-1; j>=0; j--){
+//                 if(arr[j]>=arr[i]){
+//                     curr += arr[i]; 
+//                 }
+//                 else
+//                 break;
+//             }
+
+//             for(int j = i+1; j<arr.size(); j++){
+//                 if(arr[j]>= arr[i]){
+//                     curr += arr[i];
+//                 }
+//                 else
+//                 break;
+//             }
+//             res = max(curr, res);
+//         }
+//         return res;
+//     }
+// };
+// int main()
+// {
+//     Solution sol;
+//     vector<int>v ={6,2,5,4,1,5,6};
+//     cout<<sol.largestRectangleArea(v);
+//    return 0;
+// }
+
+                            // stack
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    int largestRectangleArea(vector<int>& arr){
-        int res = 0;
-        for(int i = 0; i<arr.size(); i++){
-            int curr = arr[i];
-            for(int j = i-1; j>=0; j--){
-                if(arr[j]>=arr[i]){
-                    curr += arr[i]; 
-                }
-                else
-                break;
-            }
+class Solution {
+public:
 
-            for(int j = i+1; j<arr.size(); j++){
-                if(arr[j]>= arr[i]){
-                    curr += arr[i];
-                }
-                else
-                break;
+    vector<int>previous_Smaller(vector<int>&heights){
+        vector<int>ans;
+        stack<int>st;
+        int n = heights.size();
+        for(int i = 0; i<n; i++){
+            while(!st.empty()&&heights[st.top()]>=heights[i]){
+                st.pop();
             }
-            res = max(curr, res);
+            int ele = (st.empty())? -1: st.top();
+            ans.push_back(ele);
+            st.push(i);
+
+        }
+        return ans;
+
+    }
+    vector<int>next_Greater(vector<int>&heights){
+        vector<int>ans;
+        stack<int>st;
+        int n = heights.size();
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty()&&heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            int ele = (st.empty()) ? n:st.top();
+            ans.push_back(ele);
+            st.push(i);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int res = 0;
+        vector<int>ps = previous_Smaller(heights);
+        vector<int>ns = next_Greater(heights);
+        for(int i = 0; i<n; i++){
+            int curr  =(ns[i]-ps[i] -1) * heights[i];
+            res = max(res,curr);
         }
         return res;
     }
@@ -42,6 +103,8 @@ int main()
 {
     Solution sol;
     vector<int>v ={6,2,5,4,1,5,6};
-    cout<<sol.largestRectangleArea(v);
+    cout<<sol.largestRectangleArea(v);//10
    return 0;
 }
+
+                    
