a8d5be835b15117e34e7d2f0df25ea4f2941bd5c
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-24 02:31:21 +0530

    rotate array

---
+++ LeetCode_Solution/RotateArray.cpp
@@ -0,0 +1,62 @@
+/*
+Code
+189. Rotate Array
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+*/
+#include<bits/stdc++.h>
+using namespace std;class Solution {
+public:
+    void rotate(vector<int>& v, int k) {
+      //   vector<int>temp(v.size());
+      //   for(int i = 0; i<v.size(); i++){
+      //       temp[(i+k)%v.size()] = v[i];
+      //   }
+      //   v = temp;
+  // another approach
+        k = k % v.size();
+        reverse(v.begin(), v.end());
+        reverse(v.begin(), v.begin()+k);
+        reverse(v.begin()+k , v.end());
+    }
+};
+int main()
+{
+  Solution sol;
+  vector<int> nums = {1,2,3,4,5,6,7};
+  int k = 3;
+  sol.rotate(nums, k);
+  for(int i = 0; i<nums.size(); i++){
+   cout<<nums[i]<<" ";
+  }
+   return 0;
+}
