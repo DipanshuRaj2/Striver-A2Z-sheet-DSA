f4f4ea06d1fd2d8084de2f00fea307220894f8e8
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-02-28 22:53:33 +0530

    move zeero

--- LeetCode_Solution/Move_Zero.cpp
+++ LeetCode_Solution/Move_Zero.cpp
@@ -21,31 +21,31 @@ Constraints:
  
 
 Follow up: Could you minimize the total number of operations done?*/
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-    void moveZeroes(vector<int>& arr) {
-        int count = 0;
-        vector<int>ans;
-        for(int i = 0;i<arr.size(); i++){
-            if(arr[i] != 0){
-                ans.push_back(arr[i]);
-            }
-        }
-        for(int i = 0; i<arr.size(); i++){
-            if(arr[i]== 0){
-                ans.push_back(arr[i]);
-            }
-        }
-        int j = 0;
-        for(int i = 0; i<arr.size(); i++){
-            arr[j] =ans[i];
-            j++;
-        }
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& arr) {
+//         int count = 0;
+//         vector<int>ans;
+//         for(int i = 0;i<arr.size(); i++){
+//             if(arr[i] != 0){
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         for(int i = 0; i<arr.size(); i++){
+//             if(arr[i]== 0){
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         int j = 0;
+//         for(int i = 0; i<arr.size(); i++){
+//             arr[j] =ans[i];
+//             j++;
+//         }
         
-    }
-};
+//     }
+// };
 // int main()
 // {
 //     Solution sol;
@@ -63,9 +63,14 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
         int i = 0;
-        int j = arr.size()-1;
-        while(i<=j){
-            if(arr[i])
+        int j = 0;
+        while(i<=arr.size()&&j<arr.size()){
+            if(arr[i] != arr[j]){
+                swap(arr[i++], arr[j++]);
+            }
+            else{
+                j++;
+            }
         }
     }
 };
