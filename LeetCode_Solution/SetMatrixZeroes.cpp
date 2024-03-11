f9379215d542e0c6302223c2b73be542aad86f1a
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-03-11 11:30:52 +0530

    SetMatrixZeroes

--- LeetCode_Solution/SetMatrixZeroes.cpp
+++ LeetCode_Solution/SetMatrixZeroes.cpp
@@ -15,47 +15,103 @@ Example 2:
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]*/
 
-// bruteforce approach
+
+
+// bruteforce approach // O(n^3)
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution{
+//     public:
+//     void setZeroe(vector<vector<int>>&matrix){
+//         int n = matrix.size();
+//         cout<<n; // row size 4
+//         cout<<endl;
+//         int m = matrix[0].size();
+//         cout<<m; // col size 3
+//         cout<<endl;
+//         vector<vector<int>>visited = matrix;
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<m; j++){
+//                 if(matrix[i][j] == 0){
+//                     for(int k =0; k<m;k++){
+//                         visited[i][k] = 0;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<m; j++){
+//                 if(matrix[i][j] == 0){
+//                     for(int k = 0; k<n; k++){
+//                         visited[k][j] = 0;
+//                     }
+//                 }
+//             }
+//         }
+
+//         for(int i = 0; i<n; i++){
+//             for(int j= 0; j<m;j++){
+//                 matrix[i][j] = visited[i][j];
+//             }
+//         }
+//     }
+// };
+// int main()
+// {
+//     Solution sol;
+//     vector<vector<int>>matrix = {{1,1,1},{1,0,1},{1,1,1},{1,1,1}};
+//     sol.setZeroe(matrix);
+
+//     int n = matrix.size();
+//     int m = matrix[0].size();
+//     for(int i = 0; i<matrix.size(); i++){
+//         for(int j = 0; j<matrix[i].size(); j++){
+//             cout<<matrix[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+    
+    
+//    return 0;
+// }
+
+
+
+//better solution O(n^2)
+
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    void setZeroe(vector<vector<int>>&matrix){
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>>visited = matrix;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k =0; k<m;k++){
-                        visited[i][k] = 0;
-                    }
-                }
-            }
-        }
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k = 0; k<n; k++){
-                        visited[k][j] = 0;
-                    }
-                }
-            }
-        }
+void zeroMatrix(vector<vector<int>> &matrix){
 
-        for(int i = 0; i<n; i++){
-            for(int j= 0; j<m;j++){
-                matrix[i][j] = visited[i][j];
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int col[m] = {0};
+    int row[n] = {0};
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            if(matrix[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
-};
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m;j++){
+            if(row[i] == 1 || col[j] == 1){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
 int main()
 {
-    Solution sol;
-    vector<vector<int>>matrix = {{1,1,1},{1,0,1},{1,1,1}};
-    sol.setZeroe(matrix);
+    vector<vector<int>>matrix = {{1,1,1},{1,0,1},{1,1,1},{1,1,1}};
+    zeroMatrix(matrix);
 
     int n = matrix.size();
     int m = matrix[0].size();
@@ -65,7 +121,5 @@ int main()
         }
         cout<<endl;
     }
-    
-    
    return 0;
 }
