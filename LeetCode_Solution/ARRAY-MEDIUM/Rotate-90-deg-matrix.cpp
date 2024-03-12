31bf485d77be739a0bf15ecb197e7e184153df20
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-03-12 12:21:34 +0530

    Rotate-90-deg-matrix

---
+++ LeetCode_Solution/ARRAY-MEDIUM/Rotate-90-deg-matrix.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>>ans=matrix;
+//         for(int i = 0;i<n;i++){
+//             for(int j = 0;j<n;j++){
+//                ans[i][j] =matrix[j][i];
+//             }
+//         }
+        
+//     }
+// };
+int main()
+{
+    // Solution sol;
+    vector<vector<int>>matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    // sol.rotate(matrix);
+    int n = matrix.size();
+    for(int i = 0; i<n; i++){
+        for(int j = n-1; j>=0;j--){
+            cout<<matrix[j][i];
+        }
+        cout<<endl;
+    }
+   return 0;
+}
