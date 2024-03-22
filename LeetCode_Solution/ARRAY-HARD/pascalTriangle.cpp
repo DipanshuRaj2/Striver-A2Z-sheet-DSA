bd154294ac1c116d54fd59889448187980e687a0
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-03-22 22:34:21 +0530

    pascalTriangle

--- LeetCode_Solution/ARRAY-HARD/pascalTriangle.cpp
+++ LeetCode_Solution/ARRAY-HARD/pascalTriangle.cpp
@@ -22,34 +22,109 @@
 //    return 0;
 // }
 
-// 2nd part print the entire row 
 
+
+// 2nd part print the entire row  
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int nCr(int n, int r){
+//     long res = 1;
+//     for(int i = 0; i<r; i++){
+//         res = res * (n - i);
+//         res = res /(i+1);
+//     }
+//     return res;
+    
+// }
+// int pascalTriangle(int n , int r) {
+//         int element = nCr(n - 1, r-1);
+//         return element;
+// }
+// void printTheEntireRow(int n){
+//     for(int i = 1; i<=n; i++){
+//         cout<<pascalTriangle(n , i)<<" ";
+        
+//     }
+// }
+// int main()
+// {
+//     int n = 5;
+//     printTheEntireRow(n);
+//    return 0;
+// }
+
+// overall time complexity O(n^2) so we remove it -> 2nd part
+// #include<bits/stdc++.h>
+// using namespace std;
+// void printTheEntireRow(int n){
+//     int ans = 1;
+//     cout<< ans<<" ";
+//     for(int i = 1; i<n; i++){
+//         ans = ans * (n - i);
+//         ans = ans/i;
+//         cout<< ans<<" ";
+//     }
+// }
+// int main()
+// {
+//     int n = 5;
+//     printTheEntireRow(n);
+//    return 0;
+// }
+
+// T. COmplexity = O(n);
+
+
+// third part print the entire pascal triangle
+/*118. Pascal's Triangle
+Easy
+Topics
+Companies
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+question no. 118
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ */
 #include<bits/stdc++.h>
 using namespace std;
-int nCr(int n, int r){
-    long res = 1;
-    for(int i = 0; i<r; i++){
-        res = res * (n - i);
-        res = res /(i+1);
+class Solution {
+public:
+
+    int nCr(int n , int r){
+        long long res = 1;
+        for(int i = 0; i < r; i++){
+            res = res * (n - i);
+            res = res *(i + 1);
+        }
+        return (int)res;
     }
-    return res;
-    
-}
-int pascalTriangle(int n , int r) {
-        int element = nCr(n - 1, r-1);
-        return element;
-}
-void printTheEntireRow(int n){
-    for(int i = 1; i<=n; i++){
-        cout<<pascalTriangle(n , i)<<" ";
-        
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>>result;
+
+        for(int i = 1; row< numRows; row++){
+            vector<int>tempLst;
+            for(int j = 1; j<=i; j++){
+                tempLst.push_back(nCr(i - 1, col - 1));
+            }
+            ans.push_back(tempLst);
+        }
+        return ans;
     }
-}
+};
 int main()
 {
-    int n = 5;
-    printTheEntireRow(n);
    return 0;
 }
-
-
--- LeetCode_Solution/ARRAY-MEDIUM/SubarraySumEquals_K.cpp
+++ LeetCode_Solution/ARRAY-MEDIUM/SubarraySumEquals_K.cpp
@@ -1,26 +1,74 @@
+/*
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution{
+//     public:
+//         int subarraySum(vector<int>& nums, int k){
+//            int count = 0;
+//            for(int i = 0; i<nums.size(); i++){
+//             int sum = 0;
+//             for(int j = i; j<nums.size(); j++){
+//                 sum += nums[j];
+//                 if(sum==k){
+//                     count++;
+//                 }
+//             }
+//            }
+//             return count;
+//         }
+// };
+// int main()
+// {
+//     Solution sol;
+//     vector<int>a = {3,4,7,2,-3,1,4,2};
+//     int k = 7;
+//     cout<<sol.subarraySum(a ,k);
+//    return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
+class Solution {
     public:
-        int subarraySum(vector<int>& nums, int k){
-           int count = 0;
-           for(int i = 0; i<nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j<nums.size(); j++){
-                sum += nums[j];
-                if(sum==k){
-                    count++;
-                }
+    int subarraySum(vector<int> &nums, int k){
+        int n = nums.size();
+        int sum = 0;
+        int count = 0;
+        unordered_map<int, int>prefixSum;
+        for(int i = 0; i<n; i++){
+            sum += nums[i];
+            if(sum == k){
+                count++;
             }
-           }
-            return count;
+
+            if(prefixSum.find(sum - k) != prefixSum.end()){
+                count += prefixSum[sum - k];
+            }
+            prefixSum[sum]++;
         }
+        return count;
+    }
 };
 int main()
 {
     Solution sol;
-    vector<int>a = {3,4,7,2,-3,1,4,2};
-    int k = 7;
-    cout<<sol.subarraySum(a ,k);
+    vector<int>nums = {1,2,3,-3,1,1,1,4,2,-3};
+    int k = 3;
+    cout<<sol.subarraySum(nums, k);
    return 0;
 }
