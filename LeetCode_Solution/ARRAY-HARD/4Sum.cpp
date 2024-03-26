348601ff54c3bd45fe8919b878578d1ea21832b4
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-03-26 23:47:31 +0530

    4SUm optimal approach

--- LeetCode_Solution/ARRAY-HARD/4Sum.cpp
+++ LeetCode_Solution/ARRAY-HARD/4Sum.cpp
@@ -5,7 +5,7 @@ a, b, c, and d are distinct.
 nums[a] + nums[b] + nums[c] + nums[d] == target
 You may return the answer in any order.
 
- 
+
 
 Example 1:
 
@@ -16,25 +16,75 @@ Example 2:
 Input: nums = [2,2,2,2,2], target = 8
 Output: [[2,2,2,2]]*/
 
-//bruteforce approach
-#include<bits/stdc++.h>
+// bruteforce approach
+//  #include<bits/stdc++.h>
+//  using namespace std;
+//  class Solution{
+//      public:
+//      vector<vector<int>>fourSum(vector<int>&nums, int target){
+//          set<vector<int>>st;
+//          int n = nums.size();
+//          for(int i = 0; i<n; i++){
+//              for(int j = i+1; j<n; j++){
+//                  for(int k = j+1; k<n; k++){
+//                      for(int l = k+1;l<n; l++){
+//                          long long sum = nums[i]+nums[j]+nums[k]+nums[l];
+//                          if(sum == target){
+//                              vector<int>temp = {nums[i], nums[j], nums[k],nums[l]};
+//                              sort(temp.begin(), temp.end());
+//                              st.insert(temp);
+//                          }
+//                      }
+//                  }
+//              }
+//          }
+//          vector<vector<int>>ans(st.begin(), st.end());
+//          return ans;
+//      }
+//  };
+//  int main()
+//  {
+//      Solution sol;
+//      vector<int>nums = {1,0,-1,0,-2,2};
+//      int target = 0;
+//      vector<vector<int>>ans = sol.fourSum(nums, target);
+
+//     for(auto ele:ans){
+//         cout<<"[";
+//         for(auto it: ele){
+//             cout<<it<<", ";
+//         }
+//         cout<<"]";
+//     }
+//    return 0;
+// }
+
+// optimal approach
+#include <bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    vector<vector<int>>fourSum(vector<int>&nums, int target){
-        set<vector<int>>st;
+class Solution
+{
+public:
+    vector<vector<int>> fourSum(vector<int> nums, int target)
+    {
+        set<vector<int>> st;
         int n = nums.size();
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                for(int k = j+1; k<n; k++){
-                    for(int l = k+1;l<n; l++){
-                        long long sum = nums[i]+nums[j]+nums[k]+nums[l];
-                        if(sum == target){
-                            vector<int>temp = {nums[i], nums[j], nums[k],nums[l]};
-                            sort(temp.begin(), temp.end());
-                            st.insert(temp);
-                        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                set<long long> hashset;
+                for (int k = j + 1; k < n; k++)
+                {
+                    long long sum = nums[i] + nums[j] + nums[k];
+                    long long fourth = target - sum;
+                    if (hashset.find(fourth) != hashset.end())
+                    {
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
                     }
+                    hashset.insert(nums[k]);
                 }
             }
         }
@@ -45,16 +95,16 @@ class Solution{
 int main()
 {
     Solution sol;
-    vector<int>nums = {1,0,-1,0,-2,2};
+    vector<int>nums={1,0,-1,0,-2,2};
     int target = 0;
     vector<vector<int>>ans = sol.fourSum(nums, target);
 
-    for(auto ele:ans){
+    for(auto ele: ans){
         cout<<"[";
-        for(auto it: ele){
+        for(auto it : ele){
             cout<<it<<", ";
         }
         cout<<"]";
     }
-   return 0;
+    return 0;
 }
