f168f2987d68dd2c06a1b3c1db3790d26bf99baf
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-03 00:47:47 +0530

    important concept of unordered map, unordered set

--- LeetCode_Solution/ARRAY-EASY/intersection-Of-Two-Array.cpp
+++ LeetCode_Solution/ARRAY-EASY/intersection-Of-Two-Array.cpp
@@ -1,31 +1,62 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_set<int>st1(nums1.begin() , nums1.end());
+//         unordered_set<int>st2(nums2.begin(), nums2.end());
+
+//         vector<int>ans;
+//         for(auto ele : st1){
+//             if(st2.find(ele) != st2.end()){
+//                 ans.push_back(ele);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+// int main()
+// {
+//     Solution sol;
+//     vector<int> nums1 = {4,9,5};
+//     vector<int> nums2 = {9,4,9,8,4};
+//     vector<int>ans = sol.intersection(nums1 , nums2);
+
+//     for(auto ele : ans){
+//         cout << ele;
+//     }
+
+//    return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int>st1(nums1.begin() , nums1.end());
-        unordered_set<int>st2(nums2.begin(), nums2.end());
+vector<int>intersection(vector<int>&nums1 , vector<int>&nums2){
+    unordered_map<int, int> mpp1;
+    unordered_map<int, int> mpp2;
 
-        vector<int>ans;
-        for(auto ele : st1){
-            if(st2.find(ele) != st2.end()){
-                ans.push_back(ele);
-            }
-        }
-        return ans;
+    vector<int>ans;
+    for(int i = 0; i<nums1.size(); i++){
+        mpp1[nums1[i]]++;
     }
-};
+    for(int i = 0; i<nums2.size(); i++){
+        mpp2[nums2[i]]++;
+    }
+    for(auto it : mpp1){
+        if(mpp2[it.first]){
+            ans.push_back(it.first);
+        }
+    }
+    return ans;
+}
 int main()
 {
-    Solution sol;
-    vector<int> nums1 = {4,9,5};
+     vector<int> nums1 = {4,9,5};
     vector<int> nums2 = {9,4,9,8,4};
-    vector<int>ans = sol.intersection(nums1 , nums2);
+    vector<int>ans = intersection(nums1 , nums2);
 
     for(auto ele : ans){
         cout << ele;
     }
-
    return 0;
 }
-
