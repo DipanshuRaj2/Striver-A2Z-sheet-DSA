6931a03f1c2e69477ee7ea5de1a87d955c450ad2
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-05 23:37:44 +0530

    Missing_Repeating_Number.cpp

---
+++ CodingNinja/Array-Hard/Missing_Repeating_Number.cpp
@@ -0,0 +1,84 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int>repeatinAndMissingNumber(vector<int>&a){
+//     int missing = -1;
+//     int repeating = -1;
+
+//     for(int i = 1; i<=a.size(); i++){
+//         int cnt = 0;
+//         for(int j = 0; j<a.size(); j++){
+//             if(a[j]==i){
+//                 cnt++;
+//             }
+//         }
+//         if(cnt == 2){
+//             repeating = i;
+//         }
+//         if(cnt == 0){
+//             missing = i;
+//         }
+//     }
+//     return {repeating,missing};
+// }
+// int main()
+// {
+//     vector<int>a = {3,1,2,5,3};
+//     vector<int>ans = repeatinAndMissingNumber(a);
+//     cout<<ans[0]<<" "<<ans[1];
+//    return 0;
+// }
+
+//-------------------------------------------------O(n*n)----------------time complexity-------------------------
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int>repeatinAndMissingNumber(vector<int>&a){
+//     int n = a.size();
+//     vector<int>ans(n , 0);
+//     for(int i = 0; i<n; i++){
+//         ans[a[i]]++; // it store the count the frequency of given array in to new ans arr.
+//     }
+//     int repeating = -1, missing = -1;
+//     for(int i = 1; i<n; i++){
+//         if(ans[i] == 2){
+//             repeating = i;
+//         }
+//         else if(ans[i]== 0){
+//             missing = i;
+//         }
+//     }
+//     return{repeating, missing};
+
+// }
+// int main()
+// {
+//     vector<int>a = {3,1,2,5,3};
+//     vector<int>ans = repeatinAndMissingNumber(a);
+//     cout<<ans[0]<<" "<<ans[1];
+//    return 0;
+// }
+
+// ----------------------time complexity = --O(2n)-----------space --= --O(n)------------
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>repeatinAndMissingNumber(vector<int>&a){
+    sort(a.begin(), a.end());
+    int repeating = -1;
+    int missing = -1;
+    for(int i = 0; i<a.size()-1;i++){
+        if(a[i]==a[i+1]){
+            repeating = a[i];
+        }
+        else if((a[i] != a[i+1]-1) && (a[i] !=a[i+1])){
+            missing = a[i]+1;
+        }
+    }
+    return{repeating , missing};
+}
+int main()
+{
+    vector<int>a = {8,4,1,6,7,2,5,8};
+    vector<int>ans = repeatinAndMissingNumber(a);
+    cout<<ans[0]<<" "<<ans[1];
+   return 0;
+}
--- CodingNinja/Merge_Overlapping_Subintervals.cpp
+++ CodingNinja/Merge_Overlapping_Subintervals.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> & arr){
+    int n1 = arr.size();
+    int n2 = arr[0].size();
+    vector<vector<int>>ans;
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j<n-1; j++){
+            if(ans.size() == 0){
+             else if(arr[i][j+1] > arr[i+1][j] ){
+                ans.push_back(arr[i][j]);
+             }
+
+            }
+            else if(ans.size()!= 0){
+                else if(ans[i][j+1] > arr[i+1][j]){
+                    ans.push_back(ans[])
+                }
+            }
+        }
+    }
+    return ans;
+    
+}
 int main()
 {
+    vector<vector<int>>arr={{1, 3},{2, 4},{3, 5},{6, 7}};
+    mergeOverlappingIntervals(arr);
    return 0;
 }
