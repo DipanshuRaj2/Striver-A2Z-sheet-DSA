d727585fa77a0915e0b9a28b2d4c8790a715faf9
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-09 20:22:21 +0530

    merge sort

--- CodingNinja/Array-Hard/Merge_sort.cpp
+++ CodingNinja/Array-Hard/Merge_sort.cpp
@@ -1,44 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+void merge(vector<int> &arr, int low, int mid, int high)
 {
-   return 0;
-}
-#include<bits/stdc++.h>
-using namespace std;
-void merge(vector<int>&arr, int low, int mid, int high){
-    vector<int>temp;
     int left = low;
-    int right = mid+1;
-    while(left <= mid && right <= high){
-        if(arr[left] <= arr[right]){
+    int right = mid + 1;
+    vector<int> temp;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
             temp.push_back(arr[left]);
-            left++; 
-        } 
-        else if (arr[left] >= arr[right]) {
+            left++;
+        }
+        else if (arr[left] >= arr[right])
+        {
             temp.push_back(arr[right]);
             right++;
         }
     }
-    while(left<= mid){
-            temp.push_back(arr[left]);
-            left++;
-     }
-    while(right<= high){
-            temp.push_back(arr[right]);
-            right++;
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
     }
-    for(int i = low; i<=high;i++){
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i < temp.size(); i++)
+    {
         arr[i] = temp[i - low];
     }
 }
-    
-void mergeSort(vector<int>&arr, int low, int high) {
-    if(low == high){
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+    {
         return;
     }
-    int mid = (low+high)/2;
-    mergeSort(arr, low,mid);
-    mergeSort(arr, mid+1, high);
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
+}
+int main()
+{
+    vector<int> ans = {7, 3, 2, 4, 5, 6, 8};
+    int n = ans.size();
+    mergeSort(ans, 0, n-1);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
 }
