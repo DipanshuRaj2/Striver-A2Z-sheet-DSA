b6e6365b45bfe98dccaaefc1b3758cb522dbfa11
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-14 01:54:29 +0530

    update-upper-bound-one-line-code

--- CodingNinja/Binary Search/upper-bound.cpp
+++ CodingNinja/Binary Search/upper-bound.cpp
@@ -22,23 +22,38 @@
 
 //but we have do in the o(logn)
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int upperBound(vector<int>&arr, int x, int n){
+//     int low = 0; 
+//     int high = n-1;
+//     int ans = n;
+//     while(low <= high){
+//         int mid = (low+high)/2;
+//         if(arr[mid] > x){
+//             ans = mid;
+//             high = mid - 1;
+//         }
+//         else if(arr[mid] <= x){
+//             low = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<int>arr ={1,4,7,8,10};
+//     int n = arr.size();
+//     int x = 7;
+//     cout<< upperBound(arr, x, n);
+//    return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-int upperBound(vector<int>&arr, int x, int n){
-    int low = 0; 
-    int high = n-1;
-    int ans = n;
-    while(low <= high){
-        int mid = (low+high)/2;
-        if(arr[mid] > x){
-            ans = mid;
-            high = mid - 1;
-        }
-        else if(arr[mid] <= x){
-            low = mid + 1;
-        }
-    }
-    return ans;
+int upperBound(vector<int>arr, int x, int n){
+    int lb = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+    return lb;
 }
 int main()
 {
