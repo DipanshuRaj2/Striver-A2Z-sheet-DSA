3066fb36c5332cd25d241f3a3cb935436c52ba3b
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-19 23:27:46 +0530

    uppertriangular

--- Revision/tempCodeRunnerFile.cpp
+++ Revision/tempCodeRunnerFile.cpp
@@ -1,18 +1,35 @@
 #include<bits/stdc++.h>
-// using namespace std;
-// int fibonacciSum(int n){
+using namespace std;
 
-//    if(n <=1){
-//     return n;
-//    }
-//     int fsum = fibonacciSum(n-2);
-//     int lsum = fibonacciSum(n-1);
-//     return fsum + lsum;
-// }
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     cout<<fibonacciSum(n);
-//    return 0;
-// }
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>>nums(n, vector<int>(n));
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+         
+            cout<<"nums["<<i<<"]"<<"["<<j<<"]: ";
+            cin >> nums[i][j];
+        }
+    }
+    cout<<"print the matrix :"<<endl;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n;j++){
+            cout<<nums[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"triangular matrix :";
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = n-1;j>=i;j--){
+            cout<<"nums["<<i<<"]"<<"["<<j<<"]: ";
+            cout<<nums[i][j];
+            sum+=nums[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<sum;
+   return 0;
+}
