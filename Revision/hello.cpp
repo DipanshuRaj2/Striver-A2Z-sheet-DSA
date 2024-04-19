aa6d0ff1730ddea1b5e35f38a12963f945312328
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-19 10:46:05 +0530

    spiral matrix

--- Revision/hello.cpp
+++ Revision/hello.cpp
@@ -23,7 +23,6 @@
 //    }
 //     int fsum = fibonacciSum(n-2);
 //     int lsum = fibonacciSum(n-1);
-
 //     return fsum + lsum;
 // }
 // int main()
@@ -34,22 +33,76 @@
 //    return 0;
 // }
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int fibonacciSeries(int n ){
+//     int fib1 = 0;
+//     int fib2 = 1;
+//     for(int i = 2; i<=n; i++){
+//         int next = fib1+fib2;
+//         fib1 = fib2;
+//         fib2 = next;
+//         cout<<fib2 <<" ";
+//     }
+//     return fib2;
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout<<fibonacciSeries(n);
+//    return 0;
+// }
+
+//print the spiral matrix
+
 #include<bits/stdc++.h>
 using namespace std;
-int fibonacciSeries(int n ){
-    int fib1 = 0;
-    int fib2 = 1;
-    for(int i = 2; i<=n; i++){
-        int next = fib1+fib2;
-        fib1 = fib2;
-        fib2 = next;
+vector<int>storeSpiral(vector<vector<int>>mat){
+    vector<int>ans;
+    int n = mat.size(); // row
+    int m = mat[0].size();// col
+    
+
+    int left = 0; int right = m-1;
+    int top = 0; int bottom = n-1;
+
+    while(left<=right && top <= bottom){
+        for(int i = left; i<= right;i++){
+            ans.push_back(mat[top][i]);
+           
+        }
+         top++;
+        for(int i = top; i<=bottom; i++){
+            ans.push_back(mat[i][right]);\
+          
+        }
+          right--;
+        if(top <= bottom){
+            for(int i = right; i >= left; i--){
+                ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left <= right){
+            for(int i = bottom; i >= top;i--){
+                ans.push_back(mat[i][left]); 
+            }
+            left++;
+        }
     }
-    return fib2;
+    return ans;
+
 }
 int main()
 {
-    int n;
-    cin >> n;
-    cout<<fibonacciSeries(n);
+    vector<vector<int>>mat = {{1, 2, 3, 4},
+                             {5, 6, 7, 8},
+	                         {9, 10, 11, 12},
+		                     {13, 14, 15, 16}};
+    vector<int>ans = storeSpiral(mat);
+    for(int i = 0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
    return 0;
 }
---
+++ Revision/tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+// using namespace std;
+// int fibonacciSum(int n){
+
+//    if(n <=1){
+//     return n;
+//    }
+//     int fsum = fibonacciSum(n-2);
+//     int lsum = fibonacciSum(n-1);
+//     return fsum + lsum;
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout<<fibonacciSum(n);
+//    return 0;
+// }
