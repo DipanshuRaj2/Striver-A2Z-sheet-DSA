0c8418544a29305010cd4623072c7fb876997621
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-04-28 23:19:00 +0530

    koko-eat-banana

--- LeetCode_Solution/Binary-Search/BS on 1D Arrays/koko-eat-banana.cpp
+++ LeetCode_Solution/Binary-Search/BS on 1D Arrays/koko-eat-banana.cpp
@@ -6,7 +6,7 @@ Koko likes to eat slowly but still wants to finish eating all the bananas before
 
 Return the minimum integer k such that she can eat all the bananas within h hours.
 
- 
+
 
 Example 1:
 
@@ -36,9 +36,9 @@ Output: 23*/
 //             }
 
 //             for(int i = 1; i<=maxi; i++){
-//                 int hrs = 0; 
+//                 int hrs = 0;
 //                 for(int j = 0; j<piles.size(); j++){
-//                     int pileshour = ceil(double(piles[j])/i);
+//                     int pileshour = ceil((double)piles[j] / (double)i);
 //                     hrs = hrs +pileshour;
 //                     if(hrs > h){
 //                         break;
@@ -49,7 +49,7 @@ Output: 23*/
 //                 }
 //             }
 //         return -1;
-//     } 
+//     }
 
 // };
 // int main()
@@ -63,5 +63,59 @@ Output: 23*/
 
 // this code take O(n*n) time complexity
 
+// now i have to write a code which take complexity O(n logn);
 
-///
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int calculateTotalHours(vector<int>&piles, int hourly){
+        int totalH = 0;
+        int n = piles.size();
+        for(int i = 0; i<n; i++){
+            int hour = ceil((double)piles[i] / double(hourly));
+            totalH = totalH + hour;
+    }
+    return totalH;
+}
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int maxi = INT_MIN;
+        for (int i = 0; i < piles.size(); i++)
+        {
+            if (piles[i] > maxi)
+            {
+                maxi = piles[i];
+            }
+        }
+        int low = 1;
+        int high = maxi;
+        int ans = 0;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if (totalH > h)
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> piles = {30, 11, 23, 4, 20}; //{3,6,7,11};
+    int h = 5;
+    cout << sol.minEatingSpeed(piles, h); // output how many banana eating koko in 1 hrs;
+    return 0;
+}
