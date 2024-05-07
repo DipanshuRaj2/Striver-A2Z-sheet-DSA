17ba3290cf4586cbcf14514db6b4ad73b2352b59
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-05-07 23:59:38 +0530

    just checking

--- LeetCode_Solution/Binary-Search/BS on 1D Arrays/Kth_missing_positive_no.cpp
+++ LeetCode_Solution/Binary-Search/BS on 1D Arrays/Kth_missing_positive_no.cpp
@@ -1,95 +1,49 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// class Solution {
-// public:
-//     int findKthPositive(vector<int>& arr, int k) {
-//         vector<int>temp;
-//       for(int i = 1; i<=1000; i++){
-//             temp.push_back(i);
-//         }    
+/*
+Code
+1539. Kth Missing Positive Number
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
+
  
-//         vector<int>ans;
-//         for(int i = 0; i< 1000; i++){
-//             for(int j = 0; j<arr.size(); j++){
-//                 if(temp[i] != arr[j]){
-//                     ans.push_back(temp[i]);
-//                 }
-//             }
-//         }
-//         for(int i = 0; i<ans.size();i++){
-//             cout<<ans[i]<<" ";
-//         }
-//         return ans[k-1];
-//     }
-// };
-// int main()
-// {
-//     Solution sol;
-//     vector<int>arr= {2,3,4,7,11};
-//     int k = 5;
-//     cout<<sol.findKthPositive(arr, k);
-//    return 0;
-//
-// #include<bits/stdc++.h>
-// using namespace std;
-// void uncommonElement(vector<int> &arr, int k){
-//     vector<int>temp;
-//     for(int i = 1; i<=1000; i++){
-//         temp.push_back(i);
-//     }
 
-//     vector<int>ans;
-//     unordered_set<int>uncommon(temp.begin(), temp.end());
-//     for(int num : arr){
-//         if(uncommon.find(num) == uncommon.end()){
-//             ans.push_back(num);
-//         }
-//     }
-//     cout<<endl<<endl;
-//     for(int i = 0; i<ans.size(); i++){
-//         cout<<ans[i]<<" ";
-//     }
-    
-// }
-// int main()
-// {
-    
-//     vector<int>arr = {2,3,4,7,11};
-//     int k = 5;
-//     uncommonElement(arr, k);
-//    return 0;
-// }
+Example 1:
 
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6*/
 #include<bits/stdc++.h>
 using namespace std;
-void uncommonElement(vector<int> &arr){
-    vector<int> temp;
-    for(int i = 1; i <= 1000; i++){
-        temp.push_back(i);
-    }
-
-    vector<int> ans;
-    unordered_set<int> uncommon(temp.begin(), temp.end());
-    for(int num : uncommon){
-        if(arr.find(num) != arr.end()){
-            uncommon.erase(num);
+class Solution{
+    public:
+    int findKthPositive(vector<int>&arr, int k){
+    for(int i = 0; i<arr.size(); i++){
+        if(arr[i] <=k){
+            k = k + 1;
+            cout<<k<<endl;
         }
-        else{
-            ans.push_back(num);
-        }
+        else break;
     }
 
-    for(int i = 0; i < ans.size(); i++){
-        cout << ans[i] << " ";
+    return k;
     }
-    cout << endl;
-}
-
+};
 int main()
 {
-    vector<int> arr = {2, 3, 4, 7, 11};
-    
-    uncommonElement(arr);
-
-    return 0;
+    Solution sol;
+    vector<int>arr = {2,3,4,7,11};
+    int k = 5;
+    cout<<sol.findKthPositive(arr, k);
+   return 0;
 }
+//
