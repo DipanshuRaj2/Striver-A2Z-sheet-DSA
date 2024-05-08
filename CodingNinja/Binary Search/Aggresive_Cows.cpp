678c466310e907d20daa37c4b051118d341e455e
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-05-09 01:11:04 +0530

    Aggresive_Cows

--- CodingNinja/Binary Search/Aggresive_Cows.cpp
+++ CodingNinja/Binary Search/Aggresive_Cows.cpp
@@ -42,40 +42,121 @@ Sample Input 2 :
 
 Sample Output 2 :
 5*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool canWePlace(vector<int>& arr, int &dist, int &cows){
+//     int cntCows = 1; int live = arr[0];
+//     for(int i = 1; i<arr.size(); i++){
+//         if(arr[i] - live >= dist){
+//             cntCows++;
+//             live = arr[i];
+//         }
+//         if(cntCows >= cows){
+//             return true;;
+//         }
+//     }
+//     return false;
+// }
+// int aggressiveCows(vector<int>stalls, int cows){
+//     int n = stalls.size();
+//     sort(stalls.begin(), stalls.end());
+//     int maxi = stalls[n-1];
+//     int mini = stalls[0];
+
+//     for(int i = 1; i<= maxi - mini; i++){
+
+//         if(canWePlace(stalls, i, cows) == true){
+//             continue;
+//         }
+//         else if (canWePlace(stalls, i, cows) == false){
+//             return i - 1;
+//         } 
+//         cout<< i<<" ";
+//    }
+// }
+// int main()
+// {
+//     vector<int>arr = {0,3,4,7,10,9};
+//     int cows = 4;
+//     cout<<aggressiveCows(arr, cows);
+//    return 0;
+// }
+
+
+
+// i am trying in single function
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int aggressiveCows(vector<int>& stall, int cows){
+//     int n = stall.size();
+//     sort(stall.begin(), stall.end());
+//     int maxi = stall[n-1];
+//     int mini = stall[0];
+//     int result = -1;
+    
+//     for(int i = 1; i<=maxi - mini; i++){
+//         int cntCows = 1; int live = stall[0];
+//         for(int j = 1; j<n; j++){
+//             if(stall[j] - live >= i){
+//                 cntCows++;
+//                 live = stall[j];
+//             }
+//         }
+//         if(cntCows == cows){
+//             result = i;
+//         }
+//     }
+//     return result;
+// }
+// int main()
+// {
+//     vector<int>stalls = {0, 3, 4, 7, 10, 9};
+//     int k = 4;
+//     cout<<aggressiveCows(stalls, k);
+//     return 0;
+// }
+// overall time complexity O(n logn) + O(n^2);
+
+// optimise the code and we use binary serach
 #include<bits/stdc++.h>
 using namespace std;
-bool canWePlace(vector<int>& arr, int &dist, int &cows){
-    int cntCows = 1; int live = arr[0];
-    for(int i = 1; i<=arr.size(); i++){
-        if(arr[i] - live >= dist){
+bool canWePlace(vector<int>& stalls, int dist, int cows){
+    int cntCows = 1; int live = stalls[0];
+    for(int i = 1; i<stalls.size(); i++){
+        if(stalls[i] - live >= dist){
             cntCows++;
-            live = arr[i];
+            live = stalls[i];
         }
-        if(cntCows >= cows){
-            return true;;
+        if(cntCows >=  cows){
+            return true;
         }
     }
     return false;
 }
-int aggressiveCows(vector<int>stalls, int cows){
+int aggressiveCows(vector<int>& stalls, int cows){
     int n = stalls.size();
     sort(stalls.begin(), stalls.end());
+    int mini = 0;
     int maxi = stalls[n-1];
-    int mini = stalls[0];
-
-    for(int i = 1; i<= maxi - mini; i++){
-        if(canWePlace(stalls, i, cows) == true){
-            continue;
+    int low = stalls[0]; int high = maxi - mini;
+    int ans = -1;
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(canWePlace(stalls, mid, cows)== true){
+            ans = mid ;
+            low = mid +1;
         }
         else{
-            return i - 1;
-        } 
-   }
+            high = mid - 1;
+        }
+    }
+    return ans;
 }
 int main()
 {
-    vector<int>arr = {0,3,4,7,10,9};
-    int cows = 4;
-    cout<<aggressiveCows(arr, cows);
+    vector<int>stalls = {0, 3, 4, 7, 10, 9};
+    int k = 4;
+    cout<<aggressiveCows(stalls, k);
    return 0;
 }
