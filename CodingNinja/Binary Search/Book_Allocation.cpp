f8fe4521db50d944afc034c22343997b04eb641c
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-05-11 13:24:15 +0530

    Book allocation

--- CodingNinja/Binary Search/Book_Allocation.cpp
+++ CodingNinja/Binary Search/Book_Allocation.cpp
@@ -39,59 +39,96 @@
 // this code take more time O(n^2);
 // so we use binary search to optimise the code that is O(n logn)
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int bookAllocation(vector<int> &arr, int pages)
+// {
+//     int currentPages = 0;
+//     int cntStu = 1;
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         if (arr[i] + currentPages <= pages)
+//         {
+//             currentPages += arr[i];
+//         }
+//         else if (arr[i] + currentPages > pages)
+//         {
+//             cntStu++;
+//             currentPages = arr[i];
+//         }
+//     }
+//     return cntStu++;
+// }
+// int findPages(vector<int> arr, int n, int stu)
+// {
+//     n = arr.size();
+//     if (stu > n)
+//     {
+//         return -1;
+//     }
+//     int maxi = INT_MIN;
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         maxi = max(maxi, arr[i]);
+//         sum += arr[i];
+//     }
+//     int low = maxi;
+//     int high = sum;
+//     int ans = 0;
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+
+//         int cntStu = bookAllocation(arr, mid);
+//         if (cntStu <= stu)
+//         {
+//             ans = mid;
+//             high = mid - 1;
+//         }
+//         else if (cntStu > stu)
+//         {
+//             low = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<int> arr = {25, 46, 28, 49, 24};
+//     int m = 4;
+//     int n = arr.size();
+//     cout << findPages(arr, n, m);
+//     return 0;
+// }
+
+//using one Function
+
+#include<bits/stdc++.h>
 using namespace std;
-int bookAllocation(vector<int> &arr, int pages)
-{
-    int currentPages = 0;
-    int cntStu = 1;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (arr[i] + currentPages <= pages)
-        {
-            currentPages += arr[i];
-        }
-        else if (arr[i] + currentPages > pages)
-        {
-            cntStu++;
-            currentPages = arr[i];
-        }
-    }
-    return cntStu++;
-}
-int findPages(vector<int> arr, int n, int stu)
-{
+int findPages(vector<int>arr, int n , int m){
     n = arr.size();
-    if (stu > n)
-    {
-        return -1;
-    }
     int maxi = INT_MIN;
     int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
+    for(int i = 0; i<n; i++){
         maxi = max(maxi, arr[i]);
         sum += arr[i];
     }
-    int low = maxi;
-    int high = sum;
-    int ans = 0;
-    while (low <= high)
-    {
-        int mid = (low + high) / 2;
-
-        int cntStu = bookAllocation(arr, mid);
-        if (cntStu <= stu)
-        {
-            ans = mid;
-            high = mid - 1;
+    for(int i = maxi; i<=sum; i++){
+        int cntStu = 1; int currentPages = 0;
+        for(int j = 0; j<n; j++){
+            if(currentPages + arr[j] <= i){
+                currentPages += arr[j];
+            }
+            else if(currentPages + arr[j] > i){
+                cntStu++;
+                currentPages = arr[i]; 
+            }
         }
-        else if (cntStu > stu)
-        {
-            low = mid + 1;
+        if(cntStu == m){
+            return i;
         }
     }
-    return ans;
 }
 int main()
 {
@@ -99,5 +136,5 @@ int main()
     int m = 4;
     int n = arr.size();
     cout << findPages(arr, n, m);
-    return 0;
+   return 0;
 }
