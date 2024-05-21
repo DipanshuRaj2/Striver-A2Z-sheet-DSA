3259826ba0f5d4cece5232e4621ab82541a466b3
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-05-21 22:12:03 +0530

    message

--- Revision/tempCodeRunnerFile.cpp
+++ Revision/tempCodeRunnerFile.cpp
@@ -35,25 +35,64 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// void repeatingword(string &words){
+//     stringstream ss(words);
+//     map<string, int>ump;
+//     string word;
+//     while(ss >> word){
+//         ump[word]++;
+//     }
+//     for(auto ele: ump){
+//         if(ele.second >= 2){
+//             cout<<ele.first<<" "<<ele.second<<" "<<endl;
+//         }
+//     }
+// }
+// int main()
+// {
+//     string sentence;
+//     getline(cin , sentence);
+//     repeatingword(sentence);
+//    return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// void printtheLengthOfString(string &name){
+//     stringstream ss(name);
+//     vector<string>ans;
+//     string words;
+//     while(ss >> words){
+//         ans.push_back(words);
+//     }
+//     for(auto ele: ans){
+//         cout<<ele<<" "<<ele.length()<<endl;
+//     }
+// }
+// int main()
+// {
+//     string name = "My name is Dipanshu Raj";
+//     printtheLengthOfString(name);
+//    return 0;
+// }
 #include<bits/stdc++.h>
 using namespace std;
-void repeatingword(string &words){
-    stringstream ss(words);
-    map<string, int>ump;
-    string word;
-    while(ss >> word){
-        ump[word]++;
-    }
-    for(auto ele: ump){
-        if(ele.second >= 2){
-            cout<<ele.first<<" "<<ele.second<<" "<<endl;
+string convertString(string str){
+    for(int i = 0; i< str.length()-1; i++){
+        if(str[i] ==  ' '){
+            str[i+1] = toupper(str[i+1]);
+        }
+        else{
+            continue;
         }
     }
+    return str;
 }
 int main()
 {
-    string sentence;
-    getline(cin , sentence);
-    repeatingword(sentence);
+    string str = "I am a student of the third year";
+    cout<<convertString(str);
    return 0;
 }
