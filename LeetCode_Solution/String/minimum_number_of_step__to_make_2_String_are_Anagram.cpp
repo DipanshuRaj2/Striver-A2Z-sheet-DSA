cc6aa7584b46d64a3ccdb514f7ce84f04bbb2cbe
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-05-24 01:10:13 +0530

    minimum number of step to make 2 string are anagram by using map

--- LeetCode_Solution/String/minimum_number_of_step__to_make_2_String_are_Anagram.cpp
+++ LeetCode_Solution/String/minimum_number_of_step__to_make_2_String_are_Anagram.cpp
@@ -21,24 +21,58 @@ Example 3:
 Input: s = "anagram", t = "mangaar"
 Output: 0
 Explanation: "anagram" and "mangaar" are anagrams. */
+
+// this method using 2 array;
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution{
+//    public:
+//    int minSteps(string s, string t){
+//       int n = s.length();
+//       int mp_s[26] = {0};  // it store the every element 0;
+//       int mp_t[26] = {0};
+      
+//       for(int i = 0; i<n; i++){
+//          mp_s[s[i] - 'a']++;  // it store the frequency of every character
+//          mp_t[t[i] - 'a']++;
+//       }
+//       int result = 0;
+//       for(int i = 0; i<26; i++){
+//          if(mp_s[i] > mp_t[i]) {
+//             int countAns = mp_s[i] - mp_t[i];   // if char of string s is greater than char of string t then subtract karni hain)
+//             result += countAns;
+//          }
+//       }
+//       return result;
+//    }
+// };
+// int main()
+// {
+//    Solution sol;
+//    string s = "practice";
+//    string t = "leetcode";
+//    cout<<sol.minSteps(s, t);
+//    return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution{
    public:
    int minSteps(string s, string t){
-      int n = s.length();
-      int mp_s[26] = {0};  // it store the every element 0;
-      int mp_t[26] = {0};
-      
-      for(int i = 0; i<n; i++){
-         mp_s[s[i] - 'a']++;  // it store the frequency of every character
-         mp_t[t[i] - 'a']++;
+      unordered_map<char, int>mps;
+      unordered_map<char, int>mpt;
+      for(auto ch : s){
+         mps[ch]++;
       }
-      int result = 0;
-      for(int i = 0; i<26; i++){
-         if(mp_s[i] > mp_t[i]) {
-            int countAns = mp_s[i] - mp_t[i];   // if char of string s is greater than char of string t then subtract karni hain)
-            result += countAns;
+      for(auto ch : t){
+         mpt[ch]++;
+      }
+      int result = 0; 
+      for(char ch = 'a'; ch <= 'z'; ch++){
+         if(mps[ch] > mpt[ch]){
+            result += mps[ch] - mpt[ch];
          }
       }
       return result;
@@ -46,11 +80,10 @@ class Solution{
 };
 int main()
 {
-   Solution sol;
+      Solution sol;
    string s = "practice";
    string t = "leetcode";
    cout<<sol.minSteps(s, t);
    return 0;
 }
-
 
