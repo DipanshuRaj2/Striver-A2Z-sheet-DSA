6c1cae2eaafc6cee28357c01fe91291bac86f52b
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-06-07 09:26:58 +0530

    mesaagre today i am dong coding contiiuly

---
+++ LeetCode_Solution/String-Medium/String_to_integer.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+    public:
+    char x;
+    char z;  //it is based on the clock cycle which is large sizeof that is the clock size
+    char y;
+    
+};
+int main()
+{
+    A obj;
+    cout<<sizeof(obj);
+   return 0;
+}
