53cd7ff98ca9a3065894e33e7d432b3942c4c860
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-06-19 03:45:42 +0530

    insertion at tail

---
+++ Learn Linked_list/Doubly_Linked_List/insertion_At_Tail.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data1, Node* next1, Node* prev1){
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+    Node(){
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+Node* convert2DLL(vector<int>arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
+}
+Node* insertAtTail(Node* head, int val){
+
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+  Node* newNode = new Node(val);
+  newNode = temp -> prev;
+  newNode = te
+    return head;
+}
+void print(Node* head){
+    while(head != NULL){
+        cout<<head ->data <<"-> ";
+        head = head -> next;
+    }
+    cout<<endl;
+}
+int main(){
+    vector<int>arr = {1, 3, 4, 5};
+    Node* head = convert2DLL(arr);
+    print(head);
+    head = insertAtTail(head, 100);
+    print(head);
+    return 0;
+}
