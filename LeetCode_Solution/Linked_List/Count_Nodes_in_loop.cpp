5d95c5d30d5df147f2771390b213ca1de9132e19
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-06-23 17:01:19 +0530

    COunt the number of nodes in loop

---
+++ LeetCode_Solution/Linked_List/Count_Nodes_in_loop.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int countNodesinLoop(Node *head) {
+    Node* slow = head;
+    Node* fast = head;
+    int count = 0;
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if (slow == fast) {  // Loop detected
+        count = 1;
+            Node* temp = slow;
+            
+            while (temp->next != slow) {
+                temp = temp->next;
+                count++;
+            }
+            return count;  // Return the count of nodes in the loop
+        }
+    }
+    return 0;  // No loop found
+}
+
+int main() {
+    Node* first = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    Node* fifth = new Node(5);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = first;  // Creating a loop here
+
+    cout << countNodesinLoop(first) << endl;  // Output should be 5
+    return 0;
+}
