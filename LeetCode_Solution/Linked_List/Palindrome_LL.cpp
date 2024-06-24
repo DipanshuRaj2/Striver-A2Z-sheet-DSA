6a4aad5c2e348243f6f319e9ac87bbfa76611559
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-06-25 02:08:25 +0530

    reverse of LL

--- LeetCode_Solution/Linked_List/Palindrome_LL.cpp
+++ LeetCode_Solution/Linked_List/Palindrome_LL.cpp
@@ -1,43 +1,182 @@
+// brute force approach
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+// bool palindromeLL(Node* head){
+//     Node* temp = head;
+//     stack<int>st;
+//     while(temp !=  nullptr){
+//         st.push(temp->data);
+//         temp = temp -> next;
+//     }
+//     temp = head;
+//     while(temp != nullptr){
+//         if(st.top() != temp->data){
+//             return false;
+//         }
+//         st.pop();
+//         temp = temp -> next;
+//     }
+//     return true;
+// }
+// void print(Node* head){
+//         while(head != nullptr){
+//             cout << head -> data<<" ->";
+//             head = head -> next;
+//         }
+// }
+// int main()
+// {
+//     Node* head = new Node(1);
+//     head->next = new Node(3);
+//     head -> next -> next = new Node(3);
+//     head -> next -> next -> next = new Node(1);
+//     cout<<palindromeLL(head);
+//    return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 struct Node{
     int data;
     Node* next;
-
     Node(int data1){
         data = data1;
         next = nullptr;
     }
 };
-bool palindromeLL(Node* head){
+Node* reverseLinkedList(Node* head){
     Node* temp = head;
-    stack<int>st;
-    while(temp !=  nullptr){
-        st.push(temp->data);
-        temp = temp -> next;
-    }
-    temp = head;
+    Node* prev = nullptr;
+
     while(temp != nullptr){
-        if(st.top() != temp->data){
-            return false;
-        }
-        st.pop();
-        temp = temp -> next;
+        Node* front = temp -> next;
+        temp -> next = prev;
+        prev = temp;
+        temp = front;
     }
-    return true;
+    return prev;
 }
- void print(Node* head){
-        while(head != nullptr){
-            cout << head -> data<<" ->";
-            head = head -> next;
-        }
+bool palindromeLL(Node* head){
+    if(head == nullptr || head -> next == NULL){
+        return true;
+    }
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast -> next != nullptr && fast -> next -> next != nullptr){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+   Node* newHead = reverseLinkedList(slow -> next);
+   Node* first = head;
+   Node* second = newHead;
+
+   while(second != nullptr){
+    if(first -> data != second -> data){
+        reverseLinkedList(newHead);
+        return false;
+    }
+    first = first -> next;
+    second = second -> next;
+   
+   }
+   reverseLinkedList(newHead);
+   return true;
 }
+void print(Node* head){
+    while(head != nullptr){
+        cout << head ->data<<"->";
+        head = head ->next;
+    }
+    cout<<endl;
+}
+
 int main()
 {
     Node* head = new Node(1);
     head->next = new Node(3);
-    head -> next -> next = new Node(3);
-    head -> next -> next -> next = new Node(1);
-    cout<<palindromeLL(head);
+    head -> next -> next = new Node(4);
+    head -> next -> next -> next = new Node(3);
+    head -> next -> next -> next -> next = new Node(1);
+
+    print(head);
+    head = reverseLinkedList(head);
+    print(head);
+    if(palindromeLL(head)){
+        cout<<"Given LL is a palindrome";
+    }
+    else{
+        cout<<"Given LL is a not palindrome";
+    }
    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// insertion after head;
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1){
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+// Node* insertAfterHead(Node* head, int data){
+//     Node* temp = head->next;
+//     Node* newNode = new Node(10);
+//     head -> next  =newNode;
+//     newNode -> next =temp;
+//     return head;
+// }
+// void print(Node* head){
+//     while(head != nullptr){
+//         cout <<head -> data<<"->";
+//         head = head -> next;
+//     }
+//     cout<<endl;
+// }
+
+// int main()
+// {
+//     Node* head = new Node(1);
+//     head->next = new Node(3);
+//     head -> next -> next = new Node(3);
+//     head -> next -> next -> next = new Node(1);
+//     print(head);
+//     head = insertAfterHead(head, 10);
+//     print(head);
+
+// }
--- LeetCode_Solution/Linked_List/reverse_of_ll.cpp
+++ LeetCode_Solution/Linked_List/reverse_of_ll.cpp
@@ -14,64 +14,8 @@
 // Output: [2,1]*/
 
 // // it is brute force approach 
-#include<bits/stdc++.h>
-using namespace std;
-struct Node{
-    int data;
-    Node* next;
-
-    Node(int data1, Node* next1){
-        data = data1;
-        next = next1;
-    }
-    Node(int data1){
-        data = data1;
-        next = nullptr;
-    }
-};
-Node* reverseLL(Node* head){
-    stack<int>st;
-    Node* temp = head;
-    while(temp != nullptr){
-        st.push(temp-> data);
-        temp = temp -> next;
-    }
-    temp = head;
-    while(temp != nullptr){
-        temp->data = st.top();
-        
-        st.pop();
-        temp = temp -> next;
-    }
-    return head;
-}
-void print(Node* head){
-    Node* temp = head;
-    while(temp != NULL){
-        cout<<temp -> data<<" -> ";
-        temp = temp -> next;
-    }
-    cout<<endl;
-}
-int main()
-{
-    Node* head = new Node(1);
-    head -> next = new Node(2);
-    head -> next -> next = new Node(3);
-    head -> next -> next -> next = new Node(4);
-    head -> next -> next -> next -> next = new Node(5);
-    print(head);
-    head = reverseLL(head);
-    print(head);
-   return 0;
-}
-//2nd approach
-//without using extra space
-
-
 // #include<bits/stdc++.h>
 // using namespace std;
-
 // struct Node{
 //     int data;
 //     Node* next;
@@ -86,15 +30,20 @@ int main()
 //     }
 // };
 // Node* reverseLL(Node* head){
-//     Node* temp = head; 
-//     Node* prev = nullptr;
+//     stack<int>st;
+//     Node* temp = head;
 //     while(temp != nullptr){
-//         Node* front = temp->next;
-//         temp -> next = prev;
-//         prev = temp;
-//         temp = front;
+//         st.push(temp-> data);
+//         temp = temp -> next;
 //     }
-//     return prev;
+//     temp = head;
+//     while(temp != nullptr){
+//         temp->data = st.top();
+        
+//         st.pop();
+//         temp = temp -> next;
+//     }
+//     return head;
 // }
 // void print(Node* head){
 //     Node* temp = head;
@@ -116,6 +65,57 @@ int main()
 //     print(head);
 //    return 0;
 // }
+//2nd approach
+//without using extra space
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* reverseLL(Node* head){
+    Node* temp = head; 
+    Node* prev = nullptr;
+    while(temp != nullptr){
+        Node* front = temp->next;
+        temp -> next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
+void print(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp -> data<<" -> ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    Node* head = new Node(1);
+    head -> next = new Node(2);
+    head -> next -> next = new Node(3);
+    head -> next -> next -> next = new Node(4);
+    head -> next -> next -> next -> next = new Node(5);
+    print(head);
+    head = reverseLL(head);
+    print(head);
+   return 0;
+}
 
 
 // using recursive approach
