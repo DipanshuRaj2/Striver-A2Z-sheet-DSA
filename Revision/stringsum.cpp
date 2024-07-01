b74c3f948103d4a7c979413096a9cc56564fce7d
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-07-01 23:42:39 +0530

    messge

--- Revision/stringsum.cpp
+++ Revision/stringsum.cpp
@@ -1,20 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-// int stringsum(string s){
-//     vector<int>arr(26);
-//     arr[0] = 1;
-//     for(int i = 2; i<=26; i++){
-//         arr.push_back(i);
-//     }
-//     for(int i = 0; i<26;i++){
-//         cout<< arr[i] <<" ";
-//     }
-//     return 1;
-// }
+int stringsum(string s){
+    int sum = 0; 
+    for(int i = 0; i<s.length(); i++){
+        int charSum = s[i] - 'a' + 1;
+        sum += charSum;
+    }
+    return sum;
+}
 int main()
 {
     string s = "aba";
-
-    cout<< s[1] - 'a';
+    cout<<stringsum(s);
+    
    return 0;
 }
