03b64bea4c5a1024c4d3eccf02d507f47fa4f437
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-07-01 23:38:03 +0530

    meassage

---
+++ Revision/stringsum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int stringsum(string s){
+//     vector<int>arr(26);
+//     arr[0] = 1;
+//     for(int i = 2; i<=26; i++){
+//         arr.push_back(i);
+//     }
+//     for(int i = 0; i<26;i++){
+//         cout<< arr[i] <<" ";
+//     }
+//     return 1;
+// }
+int main()
+{
+    string s = "aba";
+
+    cout<< s[1] - 'a';
+   return 0;
+}
