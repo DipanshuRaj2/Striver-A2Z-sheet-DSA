21e28c8ed4e04959ebce737bf7318bd97d1f6f05
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-07-13 23:20:48 +0530

    Find Pairs with given sum

--- LeetCode_Solution/Double_Linked_List/Find_Pairs_with_given_sum.cpp
+++ LeetCode_Solution/Double_Linked_List/Find_Pairs_with_given_sum.cpp
@@ -17,33 +17,63 @@ Node* convert2DLL(vector<int>&arr){
     for(int i = 1; i<arr.size(); i++){
         Node* newNode = new Node(arr[i]);
         temp -> next = newNode;
-        temp -> prev = prev;
+        newNode -> prev = temp;
         temp = newNode;
     }
     return head;
 }
-vector<pair<int, int>> findPairsWithGivenSum(Node* head, int target){
-    vector<pair<int, int>>ans;
-    Node* temp = head;
-    while(temp != nullptr){
-        Node* newNode = temp -> next;
-        Node* temp1 = newNode;
-        int ele = temp -> data;
 
-        while(temp1 != nullptr){
-            if(ele + temp1 -> data == target){
-                ans.push_back(make_pair(ele, temp1->data));
-                break;
-            }
-            if(ele + temp1 -> data > target){
-                break;
-            }
-            temp1 = temp1 -> next;
-        }
+
+//  vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+//     {
+//         // code here
+//         vector<pair<int, int>>ans;
+//         Node* temp = head;
+//         while(temp != nullptr){
+            
+//             Node* nextNode = temp -> next;
+//             int ele = temp -> data;
+//             Node* temp1 = nextNode;
+//             vector<int>res;
+//             while(temp1 != nullptr){
+//                 if(ele + temp1 -> data == target){
+//                     ans.push_back(make_pair(ele, temp1->data));
+//                     break;
+//                 }
+//                 temp1 = temp1 -> next;
+//             }
+//             temp = temp -> next;
+//         }
+//    return ans;
+// }
+
+Node* findtail(Node* &head){
+    Node* temp = head;
+    while(temp -> next != nullptr){
         temp = temp -> next;
+    }
+    return temp;
+}
+vector<pair<int, int>> findPairsWithGivenSumOptimal(Node* head, int target){
+    vector<pair<int, int>>ans;
+    Node* left = head;
+    Node* right = findtail(head);
+    while(left -> data < right -> data){
+        if(left -> data + right -> data == target){
+            ans.push_back(make_pair(left-> data, right -> data));
+            left = left -> next;
+            right = right -> prev;
+        }
+        else if(left -> data + right -> data > target){
+            right = right -> prev;
+        }
+        else{
+            left = left -> next;
+        }
     }
     return ans;
 }
+
 void print(Node* head){
     while(head != nullptr){
         cout<<head -> data<<" ->";
@@ -56,7 +86,7 @@ int main(){
     vector<int>arr = {1, 2, 4, 5 ,6, 8, 9};
     Node* head = convert2DLL(arr);
     print(head);
-    vector<pair<int, int>>ans = findPairsWithGivenSum(head, 7);
+    vector<pair<int, int>>ans = findPairsWithGivenSumOptimal(head, 7);
 
     for(auto ele : ans){
         cout<<ele.first <<" "<<ele.second<<" ";
