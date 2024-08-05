07fec02930312c963b7d86774f6fe5e3732f6b15
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-06 02:38:17 +0530

    Kth Disinct String in An array

--- LeetCode_Solution/String/Kth_Distinct_String_in_An_array.cpp
+++ LeetCode_Solution/String/Kth_Distinct_String_in_An_array.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 string KthDistinct(vector<string> &arr, int k)
 {
+    /*
     unordered_map<string, int> ump;
     for (int i = 0; i < arr.size(); i++)
     {
@@ -23,6 +24,31 @@ string KthDistinct(vector<string> &arr, int k)
         if (cnt == k)
         {
             return ans[i];
+        }
+    }
+    return "";
+    */
+    unordered_map<string, bool> ump;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (ump.find(arr[i]) != ump.end())
+        {
+            ump[arr[i]] = false;
+        }
+        else{
+            ump[arr[i]] = true;
+        }
+    }
+    int cnt = 0;
+    for (string ele: arr)
+    {
+        if (ump[ele] == true)
+        {
+            cnt++;
+            if (cnt == k)
+            {
+                return ele;
+            }
         }
     }
     return "";
