1eeb23b1dd3c2e52fc56deb6a769c858069260b7
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-10 00:09:20 +0530

    insert a node in between

--- LeetCode_Solution/Linked_List_Hard.cpp/rough.cpp
+++ LeetCode_Solution/Linked_List_Hard.cpp/rough.cpp
@@ -1,35 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Node{
+struct Node
+{
     int data;
-    Node* next;
-    Node(int d){
+    Node *next;
+    Node(int d)
+    {
         data = d;
         next = nullptr;
     }
 };
-Node* convert2LL(vector<int>&arr, Node* head){
+Node *convert2LL(vector<int> &arr, Node *head)
+{
     head = new Node(arr[0]);
-    Node* temp = head;
-    for(int i = 1;i<arr.size(); i++){
-        Node* mover = new Node(arr[i]);
-        temp -> next = mover;
+    Node *temp = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *mover = new Node(arr[i]);
+        temp->next = mover;
         temp = mover;
     }
     return head;
 }
-void print(Node* head){
-    while(head != nullptr){
-        cout<<head -> data<<"->";
-        head = head -> next;
+Node *insertNode(Node *head)
+{
+    Node* temp = head;
+    while (temp != nullptr)
+    {
+        Node *copyNode = new Node(temp->data);
+        copyNode->next = temp->next;
+        temp->next = copyNode;
+        temp = temp->next->next;
     }
+    return head;
+}
+void print(Node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "nullptr";
+    cout << endl;
 }
 int main()
 {
-    Node* head = nullptr;
-    vector<int>arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 20};
-    head = convert2LL(arr,head);
+    Node *head = nullptr;
+    vector<int> arr = {7, 13, 11, 10, 1};
+    head = convert2LL(arr, head);
     print(head);
-
+    head = insertNode(head);
+    print(head);
     return 0;
 }
