b8b9dd25ad789dc0844e1439954c70d279b52d4b
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-09 23:57:22 +0530

    printClone_LL

---
+++ LeetCode_Solution/Linked_List_Hard.cpp/print_Clone_LL.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node* random;
+
+    Node(int d){
+        data = d;
+        next = nullptr;
+        random = nullptr;
+    }
+    Node(int d, Node* nextNode, Node* randomNode){
+        data = d;
+        next = nextNode;
+        random = randomNode;
+    }
+};
+Node* cloneLL(Node* head){
+    Node* temp = head;
+   unordered_map<Node*, Node*>ump;
+
+   while(temp != nullptr){
+    Node* newNode = new Node(temp->data);
+    ump[temp] = newNode;
+    temp = temp -> next;
+   }
+   cout<<"Just check the element in the map: \n";
+   for(auto ele : ump){
+    cout<<ele.first->data<<" "<<ele.second->data<<endl;
+   }
+   temp = head;
+   while(temp != nullptr){
+    Node* copyNode = ump[temp];
+    copyNode->next = ump[temp -> next];
+    copyNode -> random = ump[temp -> random];
+    temp = temp -> next;
+   }
+    return ump[head];
+}
+void printClonedLinkedList(Node* head){
+    while(head != nullptr){
+        cout<<"Data: "<<head->data;
+        if(head->random != nullptr){
+            cout<<", Random: "<<head -> random -> data;
+        } else{
+            cout <<", Random: nullptr";
+        }
+        cout<<endl;
+
+        head = head -> next;
+    }
+
+}
+int main()
+{
+  // Example Linked list: 7 -> 14 -> 21 -> -> 28
+  Node* head = new Node(7);
+  head->next = new Node(14);
+  head->next->next = new Node(21);
+  head->next->next->next = new Node(28);
+
+  //Assigning random pointers
+  head -> random = head -> next -> next;
+  head -> next -> random = head;
+  head -> next -> next -> random = head->next ->next -> next;
+  head -> next -> next -> next -> random = head -> next;
+
+  cout<<"Original Linked List with Random Pointers: "<<endl;
+  printClonedLinkedList(head);
+
+  Node* clonedList = cloneLL(head);
+  cout<<"Cloned Linked list with Random Pointer: "<<endl;
+  printClonedLinkedList(head);
+
+    return 0;
+}
