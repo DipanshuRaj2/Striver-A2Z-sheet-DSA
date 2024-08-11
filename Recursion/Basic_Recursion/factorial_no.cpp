b797d660d8be9306661f135f92086252a5ee04fb
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-11 15:57:03 +0530

    factorial_no

---
+++ Recursion/Basic_Recursion/factorial_no.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+    if(n == 1){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+int main()
+{
+    int n = 5;
+    cout<<factorial(n);
+    return 0;
+}
