7c3443477c8c901c38ea0b7305c2f6eac96f2615
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-08-20 20:16:17 +0530

    do sqrt of number  by brute force or optimal approach

--- Revision/hello.cpp
+++ Revision/hello.cpp
@@ -1,85 +1,25 @@
-
-// #include <bits/stdc++.h>
-// using namespace std;
-// int majorityEle(vector<int>&nums){
-//     int n = nums.size();
-//     int ele;
-//     int cnt = 0;
-//     for(int i = 0; i<n; i++){
-//         if(cnt == 0){
-//             cnt = 1;
-//             ele = nums[i];
-//         }
-//         else if(ele == nums[i]){
-//             cnt++;
-//         }
-//         else{
-//             cnt--;
-//         }
-//     }
-//     int cnt1 = 0;
-//     for(int i = 0; i<n; i++){
-//         if(nums[i] == ele) cnt1++;
-//     }
-//     if(cnt1 > n/2){
-//         return  ele;
-//     }
-//     return -1;
-// }
-// int main()
-// {
-//    vector<int>arr ={2, 2, 1, 1, 1, 2, 2};
-//     cout<<majorityEle(arr);
-
-//     return 0;
-// }
-
-// recursion
-
-// #include<iostream>
-// using namespace std;
-// int f1(int n){
-//     if(n==0){
-//         return 0;
-//     }
-
-//     return n + f1(n-1);
-// }
-// int main()
-// {
-//     cout<<f1(4);
-//     return 0;
-// }
-
-// reverseArray
 #include <bits/stdc++.h>
 using namespace std;
-void printArray(vector<int>arr,int i)
-{
-    if(i==arr.size())
-    {
-        // cout<<arr[i];
-        return;
+long long int floorSqrt(long long int n) {
+    long long int low = 1;
+    long long int high = n;
+    long long int ans = 0;
+
+    while (low <= high) {
+        long long int mid = (low + high) / 2;
+
+        if (mid * mid <= n) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    cout<<arr[i]<<" ";
-    printArray(arr,i+1);
+    return ans;
 }
 
-void reverseArray(vector<int>&arr, int i, int n){
-    if(i >= n/2){
-        return;
-    }
-    swap(arr[i], arr[n-i-1]);
-    reverseArray(arr, i+1, n);
-}
 int main()
 {
-    vector<int>arr = {1,2,3,4,5};
-    
-    reverseArray(arr, 0, arr.size());
-    // for(int i = 0; i<arr.size(); i++){
-    //     cout<<arr[i]<<",";
-    // }
-    printArray(arr,0);
+    cout<<floorSqrt(9883934);
     return 0;
 }
--- Revision/recursion.cpp
+++ Revision/recursion.cpp
@@ -1,23 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void print(int i, vector<int>&arr, int n){
-    if(i == n){
-        return;
+double power(double i, int n){
+    if(n == 0){
+        return 1.0;
     }
-    cout<<arr[i]<<" ";
-    print(i+1, arr, n);
-}
-void reverse(int i, vector<int>&arr, int n){
-    if(i >= n/2){
-        return;
-    }
-    swap(arr[i], arr[n-i-1]);
-    reverse(i+1, arr, n);
+    return i * power(i, n-1);
 }
 int main()
 {
-    vector<int>arr = {1,2,4,5};
-    reverse(0,arr, arr.size());
-    print(0,arr, arr.size());
+    cout<<power(2.00000, 10);
     return 0;
 }
