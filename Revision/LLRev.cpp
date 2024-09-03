e83afd10bb6227244947ae02810d7b7a342e5fc7
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-09-03 19:46:04 +0530

    rotate linked list

--- Revision/LLRev.cpp
+++ Revision/LLRev.cpp
@@ -3,103 +3,47 @@ using namespace std;
 struct Node{
     int data;
     Node* next;
+    
     Node(int d){
-        data  = d;
+        data = d;
         next = nullptr;
-    }  
+    }
 };
-Node* Convert2Arr(vector<int>&arr){
-    Node* head = new Node (arr[0]);
+Node* rotate(Node* head, int k){
     Node* temp = head;
-    for(int i = 1; i<arr.size(); i++){
-        Node* mover = new Node(arr[i]);
-        temp -> next = mover;
-        temp = mover;
-    }
-    return head;
-}
-/*
-Node* reverseLL(Node* head){
-    Node* back = nullptr;
-    Node* temp = head;
-    while(temp != nullptr){
-        Node* front = temp -> next;
-        temp -> next = back;
-        back = temp;
-        temp = front;
-    }
-    return back;
-}
-Node* addOne(Node* head){
-   head =  reverseLL(head);
-   Node* temp = head;
-   int carry = 1;
-   while(temp != nullptr){
-    if(temp ->data < 9){
-        temp -> data = temp -> data + carry;
-        carry = 0;
-    }
-    else{
-        temp -> data = 0;
-        carry = 1;
-    }
-    temp = temp -> next;
-   }
-   if(carry == 1){
-    Node* newNode = new Node(1);
-    newNode -> next = head;
-    head = newNode;
-    return head;
-   }
-   head = reverseLL(head);
-   return head;
-}
-*/
-Node* addTwoNumbers(Node* l1, Node* l2) {
-    Node* dummy = new Node(-1);
-    Node* temp = dummy;
-    Node* temp1 = l1;
-    Node* temp2 = l2;
-    int carry = 0;
-    while(temp1 != nullptr || temp2 != nullptr){
-        int sum = carry;
-        if(temp1 != nullptr){
-            sum = sum + temp1 -> data ;
-        }
-        if(temp2 != nullptr){
-            sum = sum + temp2 -> data;
-        }
-        Node* newNode = new Node(sum%10);
-        carry = sum/10;
-        temp -> next = newNode;
+    int cnt = 0;
+    Node* newHead = head;
+    Node* temp1 = head;
+    while(cnt!=k-1){
         temp = temp -> next;
-        if(temp1 != nullptr) temp1 = temp1 -> next;
-        if(temp2 != nullptr) temp2 = temp2 -> next;
+        cnt++;
     }
-    if(carry){
-        Node *newNode = new Node(carry);
-        temp -> next = newNode;
+    while(temp1 -> next != nullptr){
+        temp1 = temp1 -> next;
     }
-    return dummy->next;
-}
 
+    // cout<<"temp: "<<temp1->data<<endl;
+    newHead = temp -> next;
+    temp -> next = nullptr;
+
+    
+    temp1 -> next = head;
+    return newHead;
+}
 void print(Node* head){
     while(head != nullptr){
-        cout<<head -> data<<"->";
-        head = head -> next;
+        cout<<head-> data<<" ";
+        head = head-> next;
     }
-    cout<<"nullptr";
-    cout<<endl;
 }
-
 int main(){
-    vector<int> arr = {9,9,9,8};
-    vector<int>arr1 = {3, 2, 1};
-    Node* head1 = Convert2Arr(arr);
-    Node* head2 = Convert2Arr(arr1);
-    print(head1);
-    print(head2);
-    Node* head = addTwoNumbers(head1, head2);
+    Node* head = new Node(10);
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next->next = new Node(40);
+    head -> next -> next -> next -> next = new Node(50);
+    head -> next -> next -> next -> next -> next = new Node(60);
+   
+    head = rotate(head, 2);
     print(head);
-    return 0;
 }
--- Revision/hello.cpp
+++ Revision/hello.cpp
@@ -1,35 +1,65 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int KadaneAlgo(vector<int> &nums)
+// {
+//     int stIdx = -1;
+//     int edIdx = -1;
+//     int maxi = 0;
+//      int sum = 0;
+//      int start = -1;
+//         for(int i = 0; i<nums.size(); i++){
+//             if(sum == 0) start = i;
+//             sum += nums[i];
+//             if(sum >= maxi){
+//              maxi = max(sum, maxi);
+//              stIdx = start;
+//              edIdx = i;
+//             }
+//             if(sum <= 0){
+//                 sum = 0;
+//             }
+//         }
+//     for(int i = stIdx; i<=edIdx; i++){
+//         cout<<nums[i]<<" ";
+//     }
+//     cout<<endl;
+//     return maxi;
+// }
+// int main()
+// {
+//     vector<int> arr = {5,4,-1,7,8};
+//     cout << KadaneAlgo(arr);
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-bool checkPalindrome(string &s, int i, int j)
+vector<int> leaders(vector<int>& arr)
 {
-    if (i >= j)
-        return true;
-    if (s[i] == s[j])
+    vector<int>ans;
+    for (int i = 0; i < arr.size(); i++)
     {
-        return checkPalindrome(s, i + 1, j - 1);
-    }
-    return false;
-}
-string LongestPalindrome(string &str)
-{
-    int n = str.size();
-    int maxLength = 0;
-    int sp = 0;
-    for(int i = 0; i<n; i++){
-        for(int j = i; j<n; j++){
-            if(checkPalindrome(str, i, j)== true){
-                if(j - i + 1 > maxLength){
-                    maxLength = j - i + 1;
-                    sp = i;
-                }
+        bool leader = true;
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                leader = false;
+                break;
             }
-        }   
+        }
+        if(leader){
+            ans.push_back(arr[i]);
+        }
     }
-    return str.substr(sp, maxLength);
+    return ans;
 }
 int main()
 {
-    string str = "blockcol" ;
-    cout<<LongestPalindrome(str);
+    vector<int> arr = {63, 70, 80, 33, 33, 47, 20};
+    arr = LeadersArray(arr);
+    for(int i = 0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
