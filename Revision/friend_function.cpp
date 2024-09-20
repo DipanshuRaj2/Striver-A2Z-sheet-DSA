8687ee07a4e6197e092447eaf32210124609ade1
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-09-20 23:21:19 +0530

    reverse linked list

--- Revision/friend_function.cpp
+++ Revision/friend_function.cpp
@@ -1,51 +1,22 @@
-// #include<iostream>
-// using namespace std;
-// class Ram{
-//     int a; int b;
-//    public:
-//    Ram(){
-//         a = 2;
-//         b = 4;
-//     }
-//     friend void show(Ram r);
-// };
-
-// void show(Ram x){
-//     cout<<x.a <<" "<<x.b;
-// }
-// int main(){
-//     Ram r;
-//     show(r);
-// } // namespace std;
-
-
-
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 class A{
-    // method overloading
     public:
-    int show(int a, int b){
-        cout<<"A method: ";
-        return a+b;
-    }
-    int show(int a, int b, int c){
-        cout<<"A method 3 parameter: ";
-        return a*b* c;
+   virtual void show() = 0;
+//    virtual void show(){
+//         cout<<"Base class"<<endl;
+//     }
+};
+class B: public A{
+    public:
+    void show(){
+        cout<<"child class"<<endl;
     }
 };
-class B:public A{
-    public:
-    int show1(int a, int b){
-        cout<<"B method: ";
-        return a+b;
-    }
-};
-int main()
-{
-   B obj; 
-  obj.show(1, 4);
-
+int main(){
+    A *bptr;
+    B aa;
+    bptr = &aa;
+    bptr->show();
     return 0;
 }
-
--- Revision/hello.cpp
+++ Revision/hello.cpp
@@ -1,35 +1,78 @@
-#include<bits/stdc++.h>
-using namespace std;
-vector<int>mergeTwoArray(vector<int>&arr1, vector<int>&arr2){
-    int j = 0; 
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int>mergeTwoArray(vector<int>&arr1, vector<int>&arr2){
+//     int j = 0; 
    
-    int n = arr1.size();
-    int m = arr2.size();
-    int i = 0;
-    vector<int>ans;
-    while(i < n && j < m){
-        if(arr1[i] < arr2[j]){
-            ans.push_back(arr1[i++]);
-        }
-        else{
-            ans.push_back(arr2[j++]);
-        }
+//     int n = arr1.size();
+//     int m = arr2.size();
+//     int i = 0;
+//     vector<int>ans;
+//     while(i < n && j < m){
+//         if(arr1[i] < arr2[j]){
+//             ans.push_back(arr1[i++]);
+//         }
+//         else{
+//             ans.push_back(arr2[j++]);
+//         }
+//     }
+//     while(i < n){
+//        ans.push_back(arr1[i++]);
+//     }
+//     while(j < m){
+//         ans.push_back(arr2[j++]);
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<int>arr1 = {1, 3, 4, 5};
+//     vector<int>arr2 = {2, 4, 6, 8};
+//     vector<int>ans = mergeTwoArray(arr1, arr2);
+//     for(int i = 0; i<ans.size(); i++){
+//         cout<<ans[i]<<" ";
+//     }
+//     return 0;
+// }
+
+// linked list reverse
+ #include<bits/stdc++.h>
+ using namespace std;
+ class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int d){
+        data = d;
+        next = nullptr;
     }
-    while(i < n){
-       ans.push_back(arr1[i++]);
+ };
+ Node* reverse(Node* head){
+    Node* back = nullptr;
+    Node* temp = head;
+    while(temp != nullptr){
+        Node* front = temp -> next;
+        temp -> next = back;
+        back = temp;
+        temp = front; 
     }
-    while(j < m){
-        ans.push_back(arr2[j++]);
+    return back;
+ }
+ void print(Node* head){
+    while(head != nullptr){
+        cout<<head -> data<<"->";
+        head = head -> next;
     }
-    return ans;
-}
-int main()
-{
-    vector<int>arr1 = {1, 3, 4, 5};
-    vector<int>arr2 = {2, 4, 6, 8};
-    vector<int>ans = mergeTwoArray(arr1, arr2);
-    for(int i = 0; i<ans.size(); i++){
-        cout<<ans[i]<<" ";
-    }
-    return 0;
-}
+    cout<<"nullptr";
+    cout<<endl;
+ }
+ int main(){
+    Node* head = new Node(1);
+    head -> next = new Node(2);
+    head -> next -> next = new Node(3);
+    head -> next -> next -> next = new Node(4);
+    head -> next -> next -> next -> next = new Node(5);
+    print(head);
+    head = reverse(head);
+    print(head);
+ }
