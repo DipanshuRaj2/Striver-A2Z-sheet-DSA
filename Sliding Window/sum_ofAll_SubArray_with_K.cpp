9f72d4ffde4a075e5bf59592107f8fb16e87d1ca
dipkali <dipkali@KaliLinux.dipkali>
2024-10-08 15:49:37 +0530

    optimal approach of sum of all subarray with sum k by using sliding windows technique

--- Sliding Window/sum_ofAll_SubArray_with_K.cpp
+++ Sliding Window/sum_ofAll_SubArray_with_K.cpp
@@ -31,4 +31,34 @@ int main(){
     return 0;
 }
 */
-
+// optimal approach by using sliding windows technique
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>allSubarrayWithK(vector<int>&arr, int k){
+    vector<int>ans;
+    int start = 0; int end = 0;
+    int sum = 0;
+    while(end < arr.size()){
+        sum += arr[end];
+        if((end - start+1)< k){
+            end++;
+        }
+        else{
+            ans.push_back(sum);
+            sum -= arr[start];
+            end++;
+            start++;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>arr = {1,2,3,4,5,6};
+    int k = 3;
+    arr = allSubarrayWithK(arr, k);
+    for(int i = 0; i<arr.size(); i++){
+        cout<<arr[i]<<" "; 
+    }
+      
+    return 0;
+}
