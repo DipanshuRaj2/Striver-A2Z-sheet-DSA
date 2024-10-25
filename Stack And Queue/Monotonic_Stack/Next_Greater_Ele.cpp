7ed7f1bd12b4a2f2efc4192227620f598742c38a
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-10-26 02:06:54 +0530

    Next Greater Element

---
+++ Stack And Queue/Monotonic_Stack/Next_Greater_Ele.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+//brute force
+vector<int>nextGreaterElement(vector<int>&arr){
+    vector<int>ans(arr.size(), -1);
+    int k = 0; 
+    for(int i = 0; i<ans.size()-1; i++){
+        for(int j = i+1; j<ans.size(); j++){
+            if(arr[j] > arr[i]){
+                ans[i] = arr[j];
+                break;
+            }
+        }
+    }
+    return ans;
+}
+//optimal approach
+vector<int>nextGreaterElement1(vector<int>&arr){
+    vector<int>arr; stack<int>st;
+    for(int i = arr.size()-1; i >= 0; i--){
+        
+    }
+}
+int main(){
+    vector<int>arr = {6, 0, 8, 1, 3};
+    arr = nextGreaterElement(arr);
+    for(int i = 0; i<arr.size(); i++){
+        cout<<arr[i]<<" "  ;  
+    }
+   return 0;
+}
