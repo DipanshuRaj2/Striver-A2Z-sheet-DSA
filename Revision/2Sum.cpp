0bbb7deec8ed13d6fe81cea589e437c000e53715
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-10-27 20:27:59 +0530

    four sum brute , optimal approach using two pointer and without using extra space

--- Revision/2Sum.cpp
+++ Revision/2Sum.cpp
@@ -144,12 +144,43 @@ public:
         }
         return ans;
     }
+
+    vector<vector<int>>fourSum1(vector<int>&nums, int target){
+        vector<vector<int>>ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n-1; i++){
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            for(int j = i+1; j<n; j++){
+                if((j != i+1) && nums[j] == nums[j-1])continue;
+                int k = j+1;
+                int l = n-1;
+                while(k < l){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k]; 
+                    sum += nums[l];
+                    if(sum == target){
+                        vector<int>temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+
+                    }
+                    else if(sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+        return ans;
+    }
 };
 int main(){
     vector<int>nums = {1,0,-1,0,-2,2};
     int target = 0;
     Solution* sol = new Solution();
-    vector<vector<int>>ans = sol->fourSum(nums, target);
+    vector<vector<int>>ans = sol->fourSum1(nums, target);
     for(int i = 0; i<ans.size(); i++){
         cout<<"{";
         for(int j = 0; j<ans[i].size(); j++){
